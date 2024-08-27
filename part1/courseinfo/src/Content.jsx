25b048fe2bd642e564a326a5f87dcd4bde25f885
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-08-27 12:50:57 +0300

    Part 1 - Exercise 1.3 parts as object.

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Part from "./Part";
 
-const Content = ({ p1, e1, p2, e2, p3, e3 }) => {
+const Content = ({ p1, p2, p3 }) => {
   return (
     <div>
-      <Part part={p1} exercises={e1} />
-      <Part part={p2} exercises={e2} />
-      <Part part={p3} exercises={e3} />
+      <Part part={p1} />
+      <Part part={p2} />
+      <Part part={p3} />
     </div>
   );
 };
--- part1/courseinfo/src/Part.jsx
+++ part1/courseinfo/src/Part.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Part({ part, exercises }) {
+function Part({ part }) {
   return (
     <div>
       <p>
-        {part} {exercises}
+        {part.name} {part.exercises}
       </p>
     </div>
   );
