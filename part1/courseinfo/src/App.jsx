f0ae866606e43dec5c6c49adf90f3f6c87028ab8
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-08-27 10:39:53 +0300

    Part 1, basic version

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,30 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        p1={part1}
+        e1={exercises1}
+        p2={part2}
+        e2={exercises2}
+        p3={part3}
+        e3={exercises3}
+      />
+      <Total numberOfExercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Total = ({ numberOfExercises }) => {
+  return (
+    <div>
+      <hr />
+      <h4>Number of excersises {numberOfExercises}</h4>
+      <hr />
+    </div>
+  );
+};
+
+export default Total;
