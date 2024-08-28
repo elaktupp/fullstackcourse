3668fc020f4ae9de6387fa50fd98469a875ab941
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-08-28 17:09:45 +0300

    Part 1 - Exercises 1.13 anecdotes step 2.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>Anecdote</h1>
+      <h1>Anecdotes</h1>
       <AnecdoteGadget />
     </>
   );
@@ -11,29 +11,53 @@ function App() {
 
 export const AnecdoteGadget = () => {
   const [index, setIndex] = useState(0);
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
+  const [anecdotes, setAnecdotes] = useState([
+    { text: "If it hurts, do it more often.", votes: 0 },
+    {
+      text: "Adding manpower to a late software project makes it later!",
+      votes: 0,
+    },
+    {
+      text: "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      votes: 0,
+    },
+    {
+      text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      votes: 0,
+    },
+    { text: "Premature optimization is the root of all evil.", votes: 0 },
+    {
+      text: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      votes: 0,
+    },
+    {
+      text: "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+      votes: 0,
+    },
+    { text: "The only way to go fast, is to go well.", votes: 0 },
+  ]);
+
+  const voteAnecdote = () => {
+    const copyOfAnecdotes = [...anecdotes];
+    copyOfAnecdotes[index].votes += 1;
+    setAnecdotes(copyOfAnecdotes);
+  };
 
   return (
     <>
       <hr />
       <p>
-        <i>"{anecdotes[index]}"</i>
+        <i>"{anecdotes[index].text}"</i>
       </p>
+      <p>has {anecdotes[index].votes} votes</p>
+      <button onClick={voteAnecdote}>Vote</button>
       <button
         onClick={() => {
-          let random = Math.random(); // range from 0 to 1
-          let biggestIndex = anecdotes.length - 1;
-          let newIndex = Math.round(biggestIndex * random); // round to closes integer
-          setIndex(newIndex);
+          let r = Math.random();
+          let l = anecdotes.length - 1;
+          let i = Math.round(l * r);
+          // console.log(r, l, i, anecdotes[i]);
+          setIndex(i);
         }}
       >
         Next anecdote
