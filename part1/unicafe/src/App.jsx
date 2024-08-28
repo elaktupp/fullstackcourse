7d0d616a9800ff3c813cc5b82089b968a8fc673f
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-08-28 13:31:20 +0300

    Part 1 - Exercises 1.9 unicafe step 4, plus reset button

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,24 +5,11 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const totalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  // Good = 1, Neutral = 0, Bad = -1
-  const averageScore = () => {
-    let total = good + bad;
-    if (total === 0) total = 1;
-    return (good - bad) / total;
-  };
-
-  const isPositivePercentage = () => {
-    if (good + neutral + bad === 0) return 0;
-    else return (good / (good + neutral + bad)) * 100;
-  };
-
-  // Statistics - destructured props, functions in Statistics.
-  // Statistics2 - uses props and also functions as parameters.
+  let stats = "";
+  // Show Statistics only if there is feedback given.
+  if (good + neutral + bad > 0) {
+    stats = <Statistics good={good} neutral={neutral} bad={bad} />;
+  }
 
   return (
     <>
@@ -31,15 +18,16 @@ function App() {
         <button onClick={() => setGood(good + 1)}>good</button>
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
-        <Statistics good={good} neutral={neutral} bad={bad} />
-        <Statistics2
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={totalFeedback}
-          avgScore={averageScore}
-          positivePct={isPositivePercentage}
-        />
+        <button
+          onClick={() => {
+            setGood(0);
+            setNeutral(0);
+            setBad(0);
+          }}
+        >
+          reset
+        </button>
+        {stats}
       </div>
     </>
   );
@@ -72,22 +60,6 @@ export const Statistics = ({ good, neutral, bad }) => {
         <li>total feedback: {totalFeedback()}</li>
         <li>average score: {averageScore()}</li>
         <li>positives {isPositivePercentage()} %</li>
-      </ul>
-    </>
-  );
-};
-
-export const Statistics2 = (props) => {
-  return (
-    <>
-      <h1>Statistics</h1>
-      <ul>
-        <li>good: {props.good}</li>
-        <li>neutral: {props.neutral}</li>
-        <li>bad: {props.bad}</li>
-        <li>total feedback: {props.total()}</li>
-        <li>average score: {props.avgScore()}</li>
-        <li>positives {props.positivePct()} %</li>
       </ul>
     </>
   );
