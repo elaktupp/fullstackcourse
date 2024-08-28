2633cf08cddf796296407a24e6a5cb3a83f1c960
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-08-28 14:07:29 +0300

    Part 1 - Exercises 1.10 unicafe step 5, also corrected Statistics appearance when no feedback.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,43 +5,49 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  let stats = "";
-  // Show Statistics only if there is feedback given.
-  if (good + neutral + bad > 0) {
-    stats = <Statistics good={good} neutral={neutral} bad={bad} />;
-  }
-
   return (
     <>
       <div>
         <h1>Give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
-        <button
-          onClick={() => {
+        <Button text="good" clickHandler={() => setGood(good + 1)} />
+        <Button text="neutral" clickHandler={() => setNeutral(neutral + 1)} />
+        <Button text="bad" clickHandler={() => setBad(bad + 1)} />
+        <Button
+          text="reset"
+          clickHandler={() => {
             setGood(0);
             setNeutral(0);
             setBad(0);
           }}
-        >
-          reset
-        </button>
-        {stats}
+        />
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </>
   );
 }
+
+export const Button = ({ text, clickHandler }) => {
+  return <button onClick={clickHandler}>{text}</button>;
+};
+
+export const StatisticLine = ({ text, value, unit }) => {
+  return (
+    <div>
+      {text}: {value} {unit}
+    </div>
+  );
+};
 
 export const Statistics = ({ good, neutral, bad }) => {
   const totalFeedback = () => {
     return good + neutral + bad;
   };
 
-  // Good = 1, Neutral = 0, Bad = -1
   const averageScore = () => {
-    let total = good + bad;
+    // Good = 1, Neutral = 0, Bad = -1
+    let total = good + neutral + bad;
     if (total === 0) total = 1;
+    // Note: -bad
     return (good - bad) / total;
   };
 
@@ -50,19 +56,32 @@ export const Statistics = ({ good, neutral, bad }) => {
     else return (good / (good + neutral + bad)) * 100;
   };
 
-  return (
-    <>
-      <h1>Statistics</h1>
-      <ul>
-        <li>good: {good}</li>
-        <li>neutral: {neutral}</li>
-        <li>bad: {bad}</li>
-        <li>total feedback: {totalFeedback()}</li>
-        <li>average score: {averageScore()}</li>
-        <li>positives {isPositivePercentage()} %</li>
-      </ul>
-    </>
-  );
+  let stats = <Statistics good={good} neutral={neutral} bad={bad} />;
+
+  if (totalFeedback() > 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="total feedback" value={totalFeedback()} />
+        <StatisticLine text="average score" value={averageScore()} />
+        <StatisticLine
+          text="positives"
+          value={isPositivePercentage()}
+          unit="%"
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <i>No feedback given yet.</i>
+      </>
+    );
+  }
 };
 
 export default App;
