aeea8055b967c669e000b1de34125b8c3dc20ce3
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-02 13:23:12 +0300

    Part 2 - Exercises 2.3 course info step 8. Total with reduce. Bug fix.

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Total = ({ parts }) => {
   let numberOfExercises = 0;
-  parts.map((p) => {
-    numberOfExercises += p.exercises;
-  });
-  // NOTE: Alternative way, using reduce
-  // let total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  //   initial or return value  ^                                  ^
-  //                 of callback^                     initial value^
+  // One way to count total
+  // parts.map((p) => {
+  //   numberOfExercises += p.exercises;
+  // });
+
+  // Another way to count, using reduce. Note 0 parameter, it is initial sum.
+  numberOfExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <div>
       <hr />
-      <h4>Number of excersises {numberOfExercises}}</h4>
+      <h4>Number of excersises {numberOfExercises}</h4>
       <hr />
     </div>
   );
