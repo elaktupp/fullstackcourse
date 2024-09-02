01bd9e4680a92b153c04c561b26e50c3754c4aaf
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-02 15:00:08 +0300

    Notes frontend, example code.

---
+++ part2/notes-frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const addNote = (event) => {
+    console.log("NEW NOTE:", newNote);
+    event.preventDefault();
+    const noteObject = {
+      note: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
+    console.log("NEW NOTE:", noteObject);
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  return (
+    <div>
+      <h3>Notes</h3>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          placeholder="a new note..."
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
