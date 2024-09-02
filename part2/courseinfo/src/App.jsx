f13499706e9c2efb66f052b630730b67288b92b9
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-02 13:31:54 +0300

    Part 2 - Exercises 2.4 course info step 9.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,18 +1,31 @@
 import Course from "./components/Course";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      { id: 1, name: "Fundamentals of React", exercises: 10 },
-      { id: 2, name: "Using props to pass data", exercises: 7 },
-      { id: 3, name: "State of a component", exercises: 14 },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        { id: 1, name: "Fundamentals of React", exercises: 10 },
+        { id: 2, name: "Using props to pass data", exercises: 7 },
+        { id: 3, name: "State of a component", exercises: 14 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Node.js",
+      parts: [
+        { id: 1, name: "Routing of React", exercises: 3 },
+        { id: 2, name: "Middlewares", exercises: 7 },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
