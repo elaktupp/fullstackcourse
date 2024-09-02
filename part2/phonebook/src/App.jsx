2101560f6a3ff9ef31225836d955657315079f54
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-02 16:28:46 +0300

    Part 2 - Exercises 2.9 the phonebook step 4. Filter added.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -14,6 +18,23 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  const contactList = persons.filter((p) => {
+    // Case insensitive
+    if (newFilter.length > 0) {
+      const lowercaseName = p.name.toLowerCase();
+      const lowercaseFilter = newFilter.toLowerCase();
+      // Using less strict filter, searches filter text
+      // from the whole name, not just start.
+      return lowercaseName.includes(lowercaseFilter);
+    } else {
+      return true;
+    }
+  });
 
   const handleAddContact = (event) => {
     event.preventDefault();
@@ -32,6 +53,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with:{" "}
+        <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={handleAddContact}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -44,7 +70,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p, i) => (
+      {contactList.map((p, i) => (
         <div key={p.name}>
           {p.name} {p.number}
         </div>
