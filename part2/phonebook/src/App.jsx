7ba03cf79d2a5ccffd6d1db49ea96c349e4b8a1b
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-02 15:41:08 +0300

    Part 2 - Exercises 2.6 and 2.7 the phonebook step 1 and 2.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,39 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddContact = (event) => {
+    event.preventDefault();
+
+    if (persons.find((p) => p.name === newName)) {
+      alert(`${newName} is alreayd added to phonebook.`);
+    } else {
+      const newPerson = { name: newName };
+      console.log(newPerson);
+      setPersons(persons.concat(newPerson));
+      console.log(persons.prev);
+      setNewName("");
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddContact}>
         <div>
-          name: <input />
+          Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((p, i) => (
+        <div key={p.name}>{p.name}</div>
+      ))}
     </div>
   );
 };
