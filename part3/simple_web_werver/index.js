2b854e0fdab21f2bc7ede4f6ca2043e1473820d0
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-09 16:20:07 +0300

    Simple web server, post done

--- part3/simple_web_werver/index.js
+++ part3/simple_web_werver/index.js
@@ -68,13 +68,35 @@ app.delete("/api/notes/:id", (request, response) => {
 // API / NEW NOTE
 //
 app.post("/api/notes", (request, response) => {
-  // NOTE: Without the json-parser request.body
-  // would be undefined.
-  const note = request.body;
-  // TODO: Now just prints and returns the data.
-  console.log(note);
+  // NOTE: Without the json-parser request.body would be undefined.
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
+
+const generateId = () => {
+  console.log("GNERATE:", notes);
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+
+  // ...notes.map((n) => Number(n.id)) produces array of ids, e.g. [1,2,3]
+  // But array cannot be use as a parameter to Math.max.
+  // Array is transformed into individual number by using "..." spread syntax.
+
+  return String(maxId + 1);
+};
 
 const PORT = 3001;
 app.listen(PORT, () => {
