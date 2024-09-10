dfae27c49b3324cded434cd42804adf7ceaf2b44
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-10 08:36:03 +0300

    Part 3 - Exercises 3.1 step 1, hard coded data.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,40 @@
+// DATA
+
+let contacts = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+// CODE
+
 const express = require("express");
 const app = express();
 app.use(express.json()); // for easier json data access
 
 app.get("/", (req, resp) => {
   resp.send("<h1>Phonebook Backend</h1>");
+});
+
+app.get("/api/persons", (req, resp) => {
+  resp.send(contacts);
 });
 
 const PORT = 3001;
