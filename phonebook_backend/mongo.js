6e6de3b1159765c6cfd534e58c1aa3adb7fc3576
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-13 14:59:21 +0300

    Part 3 - Exercise 3.12, added mongoose and mongo.js

---
+++ phonebook_backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "at least password is required, and optionally new contact name and number"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv.length > 3 ? process.argv[3] : null;
+const number = process.argv.length > 4 ? process.argv[4] : null;
+
+const url = `mongodb+srv://kijutu:${password}@cluster0.kpqrp.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+// Model according to Schema
+const Contact = mongoose.model("Contact", contactSchema);
+
+if (name && number) {
+  // Name and number given, add as new contact
+  const contact = new Contact({
+    name: name,
+    number: number,
+  });
+
+  contact.save().then((result) => {
+    console.log(`Added ${contact.name} number ${contact.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  // Password only or either name or number is missing
+  Contact.find({}).then((result) => {
+    result.forEach((contact) => {
+      console.log(contact);
+    });
+    mongoose.connection.close();
+  });
+}
