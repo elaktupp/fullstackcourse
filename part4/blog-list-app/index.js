66c4864294c67649bff4ef2ed93869486794c159
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-19 16:17:59 +0300

    Part 4 - Exercise 4.2, logger in example is handy, snatched that too.

--- part4/blog-list-app/index.js
+++ part4/blog-list-app/index.js
@@ -1,6 +1,7 @@
 const app = require("./app"); // the Express app
 const config = require("./config");
+const logger = require("./logger");
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
---
+++ part4/blog-list-app/logger.js
@@ -0,0 +1,14 @@
+// Logger wapper makes it easy to switch logging system,
+// just do changes here.
+const info = (...params) => {
+  console.log(...params)
+}
+
+const error = (...params) => {
+  console.error(...params)
+}
+
+module.exports = {
+  info,
+  error,
+}
