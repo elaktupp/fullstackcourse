9bf6dd10dee2bbaae9a6497563afb06d634c2538
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-20 13:44:26 +0300

    Part 4 - Exercise 4.4, helper functions and unit tests, totalLikes, step 2.

--- part4/blog-list-app/tests/list_helper.test.js
+++ part4/blog-list-app/tests/list_helper.test.js
@@ -2,9 +2,52 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
 
-  const result = listHelper.dummy(blogs);
-  assert.strictEqual(result, 1);
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  const listWithBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d1700",
+      title: "Whatever",
+      author: "Jonne",
+      url: "https://dummy/jonne/whatever.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d1711",
+      title: "Duh!",
+      author: "Homer",
+      url: "https://dummy/homer-os/duh.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("when list has 3 blogs, each with 5 likes, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithBlogs);
+    assert.strictEqual(result, 15);
+  });
 });
--- part4/blog-list-app/utils/list_helper.js
+++ part4/blog-list-app/utils/list_helper.js
@@ -1,7 +1,11 @@
-const dummy = (blogs) => {
-  return 1;
+const totalLikes = (blogs) => {
+  //
+  const cbSumUpLikes = (result, blog) => {
+    return result + blog.likes;
+  };
+  // result contains sum of likes from earch blog
+  // blog is next blog of which likes are added to result
+  return blogs.reduce(cbSumUpLikes, 0);
 };
 
-module.exports = {
-  dummy,
-};
+module.exports = { totalLikes };
