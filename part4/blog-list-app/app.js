8c4f9f18edf4d1adf0c969527fdb580aef511742
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-20 09:34:33 +0300

    Part 4 - Exercise 4.2, blogs router added.

--- part4/blog-list-app/app.js
+++ part4/blog-list-app/app.js
@@ -2,31 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const logger = require("./logger");
 const config = require("./config");
 const middleware = require("./middleware");
+const blogsRouter = require("./blogs");
 
-const Blog = require("./blog");
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
+logger.info("connecting to", config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
