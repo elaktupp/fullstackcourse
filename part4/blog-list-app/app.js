5060f8f227fc502f971b43bd621fe5779a64475f
Kimmo Tuppela <kimmo.tuppela@symbio.com>
2024-09-20 09:17:45 +0300

    Part 4 - Exercise 4.2, middleware from earlier example with request logger and error handlers.

--- part4/blog-list-app/app.js
+++ part4/blog-list-app/app.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./config");
+const middleware = require("./middleware");
+
 const Blog = require("./blog");
 
 const mongoUrl = config.MONGODB_URI;
@@ -10,6 +12,7 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -24,5 +27,8 @@ app.post("/api/blogs", (request, response) => {
     response.status(201).json(result);
   });
 });
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
